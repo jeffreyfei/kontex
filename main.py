391f5c19de70ba42ce66d2f4063d9d9bd318f838
William Lo <will-i-am_95@hotmail.com>
2017-12-10 00:32:35 -0500

    adds sentence similarity to title measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 from nltk.tokenize import sent_tokenize
 
 from constants import STOPWORDS
@@ -17,16 +17,16 @@ def transform_tfidf(words):
     tfidf_transformer = TfidfTransformer()
     return tfidf_transformer.fit_transform(words)
 
-def find_avg_tfidf(tfidf_array):
+def find_avg_tfidf(tfisf_array):
     '''
     averages out the tfidf values of every sentence,
     returns a list of averages by sentence position
     '''
     sentence_avg_tfidf = []
-    for sentence in tfidf_array:
+    for sentence in tfisf_array:
         total = 0
-        for tf_idf_val in sentence:
-            total += tf_idf_val
+        for tf_isf_val in sentence:
+            total += tf_isf_val
         avg = total / len(sentence)
         sentence_avg_tfidf.append(avg)
     return sentence_avg_tfidf
@@ -39,17 +39,23 @@ def find_max_sentence_length(sentences):
             max_length = len(s)
     return max_length
 
+
+def find_title_similarity_measure(title, sentences):
+    tfidf_transformer = TfidfVectorizer()
+    title_and_sentences = [title] + sentences
+    tfidf = tfidf_transformer.fit_transform(title_and_sentences)
+    pairwise_similarity = tfidf * tfidf.T
+    return pairwise_similarity.A[0]
+
 def main(documents):
     for document in documents:
         title, body = pop_subject_from_document(document)
-        print title
         paragraphs = [p for p in body.split('\n') if p]
         for paragraph in paragraphs:
-            print paragraph
             sentences = sent_tokenize(paragraph)
             sentence_data = []
             word_tokens = tokenize(sentences)
-
+            sentence_title_similarities = find_title_similarity_measure(title, sentences)
             # start processing different attributes of every sentence
             sentence_tf_isf = find_avg_tfidf(transform_tfidf(word_tokens).toarray())
             max_sentence_length = find_max_sentence_length(sentences)
@@ -62,6 +68,7 @@ def main(documents):
                     'avg_tf_isf': sentence_tf_isf[i],
                     'len_ratio': sentence_length,
                     'pos': sentence_pos,
+                    'simlarity_to_title': sentence_title_similarities[i]
                 })
             print sentence_data
 
