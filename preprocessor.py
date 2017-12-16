318d8b65bec2b5bb262bf995eb1549f0420812e6
William Lo <will-i-am_95@hotmail.com>
2017-12-16 17:03:53 -0500

    switches dataset to kaggle news articles

--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import *
+import csv
 
 class LancasterTokenizer(object):
     def __init__(self):
@@ -9,19 +10,19 @@ class LancasterTokenizer(object):
         return [self.ls.stem(word) for word in word_tokenize(doc)]
 
 
-def find_beginning_of_body(split_document):
-    for line, i in enumerate(split_document):
-        if line == '':
-            return i + 1
-        else:
-            return 0 # fuckit
-
-
-def pop_subject_from_document(document):
-    split_document = document.split("\n")
-    header = split_document[1]
-    body_begin = find_beginning_of_body(split_document)
-    body = ' '.join(split_document[body_begin:])
-    title_begin = len("Subject: ")
-    title = header[title_begin:]
-    return title, body
+def fetch_datasets():
+    data = []
+    csv.field_size_limit(500 * 1024 * 1024)
+    i = 0
+    with open('articles1.csv', 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            i += 1
+            sanitized_body = row['content'].decode('utf-8').encode('ascii', errors='ignore')
+            data.append({
+                'header': row['title'],
+                'body': sanitized_body,
+            })
+            if i > 100:
+                break
+    return data
